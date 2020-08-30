db92effc05c5c730ee4eaa9f47b2284df49939a9
HJ94 <50905731+HJ94@users.noreply.github.com>
2020-08-30 15:52:38 +0900

    for문(1부터 100까지의 합)

---
+++ ForEx.java
@@ -0,0 +1,26 @@
+package appEx;
+
+public class ForEx {
+
+	public static void main(String[] args) {
+//		  1부터 100까지의 합 
+		  
+		int sum=0; 
+	
+		  for(int i=1; i<=100; i++){ 
+			  sum = sum+i; 
+		  }
+		  System.out.println(sum);
+		
+		
+		
+		/*
+		 * for(int i=1; i<=9; i++) { for(int j=1; j<=9; j++) { System.out.println();
+		 * 
+		 * }
+		 * 
+		 * }
+		 */
+		
+	}
+}
