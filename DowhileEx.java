648ba728ef46d0c7c1644504648e5bf716036acd
HJ94 <50905731+HJ94@users.noreply.github.com>
2020-08-31 20:50:57 +0900

    do-while문(1부터10출력)

---
+++ DowhileEx.java
@@ -0,0 +1,21 @@
+package appEx;
+
+/*반복문(do ~ while)
+
+do {
+	 실행구문;
+}while(조건식);*/
+
+
+public class DowhileEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int cnt =0;
+		do {
+			cnt++;
+			System.out.println(cnt);
+		}while(cnt < 10);
+		
+	}
+}
