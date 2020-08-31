764fccc26feba912e89877046a1c7cb17a047f9d
HJ94 <50905731+HJ94@users.noreply.github.com>
2020-08-31 20:52:24 +0900

    do-while(break문)

---
+++ DoWhileBreakEx.java
@@ -0,0 +1,28 @@
+package appEx;
+
+public class DoWhileBreakEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 0;
+		boolean result = true;
+		
+		do {
+			i++;
+			if(result) {
+				System.out.println("실행 구문: " + i);
+				if(i == 5) {
+					result = false;
+				}
+				if(!result) {
+					System.out.println("실행 구문 종료합니다.");
+					break;
+				}
+			}else {
+				
+				System.out.println("i : " + i);
+			}	
+		}while(i < 10);
+	}
+
+}
