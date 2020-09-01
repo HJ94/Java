ccae51b75e6b18d352883f9e27417085c55a0f08
HJ94 <50905731+HJ94@users.noreply.github.com>
2020-09-01 23:06:42 +0900

    별찍기-1

---
+++ Star.java
@@ -0,0 +1,18 @@
+package appEx;
+
+public class Star {
+
+	public static void main(String[] args) {
+		for (int i = 5; i > 0; i--) {
+			for (int j = 5; j <= 0; j--) {
+				if (i > j) {
+					System.out.print(" ");
+				} else {
+					System.out.print("*");
+				}
+			}
+			System.out.println();
+		}
+	}
+
+}
